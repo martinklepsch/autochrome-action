d7bd7519856d446bb88200d6cf889141620dd8cd
Martin Klepsch <martinklepsch@googlemail.com>
2020-03-16 11:54:38 +0100

    remove git status stuff from docker entrypoint

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-cd /github/workspace
-git status
-git diff 71f74d92c60738db7257230ec7cfb8de681ac6c3 e7abf5cfd9fbb849c973790dc21c333717df4b2f
-
 cd /action/lib
 clojure --report stderr run.clj /github/workspace
