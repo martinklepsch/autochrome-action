2a05958b7cb66dd5bfe80713130ee03ebb219d81
Martin Klepsch <martinklepsch@googlemail.com>
2020-03-16 11:36:51 +0100

    no pushd, ugh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-pushd /github/workspace
+cd /github/workspace
 git diff 71f74d92c60738db7257230ec7cfb8de681ac6c3 e7abf5cfd9fbb849c973790dc21c333717df4b2f
-popd
 
 cd /action/lib
-
 clojure --report stderr run.clj /github/workspace
