73c88df96f1abc75693c168ed294c5fc2f74994e
Martin Klepsch <martinklepsch@googlemail.com>
2020-03-16 11:33:37 +0100

    try to figure out why local raw diff is emtpy

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+pushd /github/workspace
+git diff 71f74d92c60738db7257230ec7cfb8de681ac6c3 e7abf5cfd9fbb849c973790dc21c333717df4b2f
+popd
+
 cd /action/lib
 
 clojure --report stderr run.clj /github/workspace
